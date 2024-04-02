80b5bb36f89aa42991a4d99dc4612281d15d56be
shilu10 <shilu842003@gmail.com>
2024-04-02 14:12:00 +0530

    feat: added redis and s3 capability to microservice1

--- src/microservice-1/app/configs/base.py
+++ src/microservice-1/app/configs/base.py
@@ -3,5 +3,3 @@ import os
 
 class BaseConfig:
     video_directory = "uploads"
-    aws_access_key = os.environ["ACCESS_KEY"]
-    aws_secret_access_key = os.environ["SECRET_ACCESS_KEY"]
--- src/microservice-1/app/routes/producer_route.py
+++ src/microservice-1/app/routes/producer_route.py
@@ -1,8 +1,10 @@
+import uuid
+import boto3
+import redis
+import os 
 from flask import Blueprint, jsonify, render_template, request
 from marshmallow import ValidationError
 from werkzeug.utils import secure_filename
-import boto3
-import os 
 from ..schema.metadata_schema import MetadataSchema
 from ..configs import Config
 
@@ -10,9 +12,13 @@ from ..configs import Config
 producer_bp = Blueprint("producer_bp",  __name__)
 metadata_schema = MetadataSchema()
 s3_client = boto3.client('s3', 
-                         aws_access_key_id=os.environ["ACCESS_KEY"],
+                        aws_access_key_id=os.environ["ACCESS_KEY"],
                         aws_secret_access_key=os.environ["SECRET_KEY"]
                     )
+
+redis_client = redis.Redis(host = os.environ["REDIS_HOST"], 
+                           port=os.environ["REDID_PORT"]
+                        )
 
 @producer_bp.route("/post_video", methods=["POST"])
 def post_message():
@@ -31,19 +37,36 @@ def post_message():
     
     # 3. Save it (or upload to S3, etc.)
     filename = secure_filename(video_file.filename)
-    save_path = os.path.join(Config.video_directory, filename)
-    video_file.save(save_path)
+    #save_path = os.path.join(Config.video_directory, filename)
+    #video_file.save(save_path)
 
     # saving it into s3
-    response = s3_client.create_bucket(
-        Bucket = filename,
-        CreateBucketConfiguration = {
-            'AZ': 'private',
-            'LocationConstraint': 'eu-west-1',
-        },
-    )
+    try:
+        response = s3_client.put_object(
+            Body = video_file,
+            Bucket = 'k8s-scalers-example-bucket',
+            Key = filename,
+        )
 
-    
+        job_id = str(uuid.uuid4())
+
+        # job status
+        status_dict = dict()
+        status_dict["job_id"] = job_id
+        status_dict["status"] = "Processing"
+        status_dict["progress"] = 0
+
+        redis_client.hset(job_id, status_dict)
+
+    except Exception as err:
+        return jsonify(
+            {
+                "sucsess": False,
+                "reason": err
+            }
+        ), 400
+
     return jsonify({
-        "message": "success"
+        "message": "success",
+        "job_id": job_id
     }), 200
